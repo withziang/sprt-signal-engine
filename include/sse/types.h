323ab99a0ab46ab0dd1c409034af9e32b57c16fd
Ziang Wang <zach0623@icloud.com>
2025-12-25 22:56:38 -0500

    fix: order bug

--- include/sse/types.h
+++ include/sse/types.h
@@ -3,20 +3,6 @@
 #include <cstdint>
 
 namespace sse{
-    struct IngestInput{
-        std::string website;
-        std::int64_t timestamp;
-        std::string league;
-        std::string game_name;
-        std::string team1;
-        std::string team2;
-        sse::BetType bet_type;
-        double team1_line;
-        double team1_odd;
-        double team2_line;
-        double team2_odd;
-    };
-
     enum class BetType{
         Moneyline,
         Spread,
@@ -27,5 +13,19 @@ namespace sse{
         Accepted,
         RejectedInvalid,
         RejectedStale
+    };
+    
+    struct IngestInput{
+        std::string website;
+        std::int64_t timestamp;
+        std::string league;
+        std::string game_name;
+        std::string team1;
+        std::string team2;
+        BetType bet_type;
+        double team1_line;
+        double team1_odd;
+        double team2_line;
+        double team2_odd;
     };
 }
